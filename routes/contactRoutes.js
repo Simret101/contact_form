e43efe29e501a7437c2fc36cd755a2e01ed53819
Simret <semretb74@gmail.com>
2025-04-06 10:20:32 +0300

    feat:add name

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { validateContact } = require("../middleware/validateRequest");
 const { handleContact } = require("../controllers/contactController");
 
-// No rate limiter here; it's in the controller
+
 router.post("/contact", validateContact, handleContact);
 
 module.exports = router;
